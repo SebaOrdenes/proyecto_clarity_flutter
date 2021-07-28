90cb7f069aeadb0a8643b136412f499c20ef9d70
Seba <sebastian.ordenes@usach.cl>
2021-07-28 17:37:07 -0400

    Arreglos parentwidget

--- lib/Screens/Informacion/components/body.dart
+++ lib/Screens/Informacion/components/body.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_auth/Screens/Admin/components/menu_lateral.dart';
 import 'package:flutter_auth/Screens/Layout/components/background.dart';
 import 'package:flutter_auth/Screens/Lecturas/lectura_screen.dart';
+import 'package:flutter_auth/Screens/Tratamiento/tratamiento_screen.dart';
 import 'package:flutter_auth/models/Lecturas.dart';
+
+import '../../../constants.dart';
 
 //import 'package:flutter_auth/Screens/Login/login_screen.dart';
 
@@ -27,6 +31,15 @@ class _BodyState extends State<Body> {
     );
   }
 
+//Aumenta el contador de la imagen
+  void _addImagen() {
+    for (var i = 0; i < 6; i++) {
+      images.add(i + 1);
+      //Permite el cambio de estado de las imagenes (reflejan en lista los nuevos elementos)
+      setState(() {});
+    }
+  }
+
   //initState se ejecuta antes que se carguen todos los componentes
   @override
   void initState() {
@@ -39,41 +52,67 @@ class _BodyState extends State<Body> {
   }
 
   Widget build(BuildContext context) {
-    //Size size = MediaQuery.of(context).size;
-    // This size provide us total height and width of our screen
-    return Background(
+    Size size = MediaQuery.of(context).size;
+    //return SafeArea(child: Center(child: ListView(children: [])));
+    return SafeArea(
+        child: Center(
       child: ListView.builder(
         itemCount: images.length,
         itemBuilder: (BuildContext context, int index) {
-          //POR AHORA se pondrán imagenes de referencia solamente
           return Container(
-              //DESPUES hay que cambiarlo por boton hacia una vista correspondiente a cada lectura
-              padding: EdgeInsets.all(20.0),
-              child: Positioned(
-                child: InkWell(
-                  onTap: () {
-                    //dondeClickeo(index);
-                    getLect(index);
-                  },
-                  child: Container(
-                    child: Image.asset(
-                      'assets/images/Banner${images[index]}.jpg',
-                      height: 300,
-                    ),
-                  ),
+            padding: EdgeInsets.all(20.0),
+            child: GestureDetector(
+              onTap: () {
+                //dondeClickeo(index);
+                getLect(index);
+              },
+              child: Container(
+                child: Image.asset(
+                  'assets/images/Banner${images[index]}.jpg',
+                  height: 300,
                 ),
-              ));
+              ),
+            ),
+          );
         },
       ),
-    );
-  }
+    ));
 
-  //Aumenta el contador de la imagen
-  void _addImagen() {
-    for (var i = 0; i < 6; i++) {
-      images.add(i + 1);
-      //Permite el cambio de estado de las imagenes (reflejan en lista los nuevos elementos)
-      setState(() {});
-    }
+    // This size provide us total height and width of our screen
+    /*
+    return Scaffold(
+      drawer: MenuLateral(),
+      body: Column(
+        children: <Widget>[
+          Flexible(
+            child: Container(
+              //width: double.infinity,
+              child: ListView.builder(
+                itemCount: images.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                      padding: EdgeInsets.all(20.0),
+                      child: Positioned(
+                        child: InkWell(
+                          onTap: () {
+                            //dondeClickeo(index);
+                            getLect(index);
+                          },
+                          child: Container(
+                            child: Image.asset(
+                              'assets/images/Banner${images[index]}.jpg',
+                              height: 300,
+                            ),
+                          ),
+                        ),
+                      ));
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+      backgroundColor: kWhiteColor,
+    );*/
   }
 }
